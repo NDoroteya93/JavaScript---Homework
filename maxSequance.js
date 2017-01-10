db2f6a388ad0cab21230524c62bae5342ef81330
Doroteya <doroteanikolova93@gmail.com>
2017-01-10 22:20:25 +0200

    Fixed max sequance.

--- maxSequance.js
+++ maxSequance.js
@@ -2,29 +2,21 @@
 
 let arr = ['10', '2', '1', '1', '2', '3', '3', '2', '2', '2', '1'];
 
-function maximalSequance(arr)
-{
+function maximalSequance(arr) {
     let N = +arr.shift();
-    let result = [];
+    let array = arr.map(Number);
     let count = 1;
-    let lastCount;
-    if (N >= 1 && N <= 1024)
-    {
-        for (let i = 0; i < N; i += 1)
-        {
-            if (arr[i] === arr[i - 1])
-            {
-                count += 1;
-            } else
-            {
-                if (lastCount < count)
-                {
-                    result[0] = count;
-                }
-                lastCount = count;
-                count = 1;
+    let result = 0;
+    for (let i = 0; i < N; i += 1)
+        if (array[i] === array[i + 1]) {
+            count += 1;
+            if (count > result) {
+                result = count;
             }
+        } else {
+            count = 1;
         }
-    }
-    console.log(result[0]);
+    console.log(result);
 }
+
+maximalSequance(arr)
