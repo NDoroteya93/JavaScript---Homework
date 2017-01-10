c488c0aab7ccb20a372e3b06eedf01a6305effb3
Doroteya <doroteanikolova93@gmail.com>
2017-01-10 21:54:31 +0200

    Fixed binarySearch();

--- binarySearch.js
+++ binarySearch.js
@@ -2,34 +2,28 @@
 
 let arr = ['10', '1', '2', '4', '8', '16', '31', '32', '64', '77', '99', '32'];
 
-function binarySearch(arr)
-{
-    const N = +arr.shift();
-    const X = arr.pop();
-    const mid = Math.floor(N / 2);
-    let sortArr = arr.sort((a, b) => a - b);
-    if (X === sortArr[mid])
-    {
-        console.log(mid);
-    } else if (X > sortArr[mid])
-    {
-        for (let i = mid; i < N; i += 1)
-        {
-            if (X === sortArr[i])
-            {
-                console.log(i);
-                break;
-            }
+function binarySearch(arr) {
+    let array = arr.map(Number),
+        N = array.shift(),
+        X = array.pop(),
+        sortArr = array.sort((a, b) => a - b),
+        start = 0,
+        end = sortArr.length - 1,
+        mid = 0,
+        currentElement;
+    while (start <= end) {
+        mid = (start + end) / 2 | 0;
+        currentElement = sortArr[mid];
+        if (X === sortArr[0]) {
+            return '0';
         }
-    } else
-    {
-        for (let i = mid; i > 0; i -= 1)
-        {
-            if (X === sortArr[i])
-            {
-                console.log(i);
-                break;
-            }
+        if (currentElement < X) {
+            start = mid + 1;
+        } else if (currentElement > X) {
+            end = mid - 1;
+        } else {
+            return mid;
         }
     }
+    return -1;
 }
