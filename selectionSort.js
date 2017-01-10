03aa0a04b806cddcc885eb728afb0a26b8304bfb
Doroteya <doroteanikolova93@gmail.com>
2017-01-10 22:43:29 +0200

    Fixed selectionSort

--- selectionSort.js
+++ selectionSort.js
@@ -2,22 +2,21 @@
 
 let arr = ['10', '36', '10', '1', '34', '28', '38', '31', '27', '30', '20'];
 
-function selectionSort(arr)
-{
+
+function selectionSort(arr) {
     let N = +arr.shift();
-    for (let i = 0; i < N; i += 1)
-    {
-        for (let j = i + 1; j < N; j += 1)
-        {
-            if (arr[i] > arr[j])
-            {
-                let temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+    let array = arr.map(Number);
+    for (let i = 0; i < N; i += 1) {
+        for (let j = i + 1; j < N; j += 1) {
+            if (array[i] > array[j]) {
+                let temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
             }
         }
     }
-    arr.forEach(x => console.log(x));
+    array.forEach(x => console.log(x));
 }
+
 
 selectionSort(arr);
